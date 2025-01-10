4f1948ecf180057c03c18ce9af47ee98600b7972
patratiip <154238580+patratiip@users.noreply.github.com>
2025-01-10 13:30:11 +0100

    commented code was deleted

--- lib/app_runner.dart
+++ lib/app_runner.dart
@@ -1,14 +1,12 @@
 import 'dart:async';
 
-import 'package:async_redux/async_redux.dart';
 import 'package:bloc_concurrency/bloc_concurrency.dart' as bloc_concurrency;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:procrastinator/business.dart';
 import 'package:procrastinator/firebase_options.dart';
-import 'package:procrastinator/main.dart';
-import 'package:procrastinator/src/app_student/features/entry_adding/redux_app_state.dart';
+
 import 'package:procrastinator/src/core/constant/config.dart';
 import 'package:procrastinator/src/core/di/composition_root.dart';
 import 'package:procrastinator/src/core/utils/app_bloc_observer.dart';
@@ -51,9 +49,6 @@ final class AppRunner {
     await Future.wait([
       Business.init(),
     ]);
-
-    // Dependencies initializing
-    // getIt.registerSingleton<AppModel>(AppModelImplementation());  // Example
 
 ////////////////////////////////////////////////////
 
